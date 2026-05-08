c9a91619b5479f886dac7aab45089e30e1892239
Muhammed-Mahmoud-Gouda <muhammedmahmoudgoda66@gmail.com>
2026-05-08 16:22:53 +0300

    Edit PaginationHelper

--- BookStore.BLL/Helper/PaginationHelper.cs
+++ BookStore.BLL/Helper/PaginationHelper.cs
@@ -1,30 +1,43 @@
 ﻿namespace ShopNest.BLL.Helpers
 {
-    public class PaginationHelper<T>
+    using Microsoft.EntityFrameworkCore;
+
+    namespace ShopNest.BLL.Helpers
     {
-        public List<T> Items { get; set; } = new();
-        public int TotalItems { get; set; }
-        public int CurrentPage { get; set; }
-        public int PageSize { get; set; }
-        public int TotalPages => (int)Math.Ceiling((double)TotalItems / PageSize);
-        public bool HasNext => CurrentPage < TotalPages;
-        public bool HasPrevious => CurrentPage > 1;
-
-        public static PaginationHelper<T> Create(IEnumerable<T> source, int page, int pageSize)
+        public class PaginationHelper<T>
         {
-            var totalItems = source.Count();
-            var items = source
-                .Skip((page - 1) * pageSize)
-                .Take(pageSize)
-                .ToList();
+            public List<T> Items { get; set; } = new();
 
-            return new PaginationHelper<T>
+            public int TotalItems { get; set; }
+
+            public int CurrentPage { get; set; }
+
+            public int PageSize { get; set; }
+
+            public int TotalPages =>
+                (int)Math.Ceiling((double)TotalItems / PageSize);
+
+            public bool HasNext => CurrentPage < TotalPages;
+
+            public bool HasPrevious => CurrentPage > 1;
+
+            public static async Task<PaginationHelper<T>> CreateAsync(IQueryable<T> source,int page,int pageSize)
             {
-                Items = items,
-                TotalItems = totalItems,
-                CurrentPage = page,
-                PageSize = pageSize
-            };
+                var totalItems = await source.CountAsync();
+
+                var items = await source
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
+
+                return new PaginationHelper<T>
+                {
+                    Items = items,
+                    TotalItems = totalItems,
+                    CurrentPage = page,
+                    PageSize = pageSize
+                };
+            }
         }
     }
 }
